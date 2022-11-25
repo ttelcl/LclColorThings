aaa8002e8afff3279a9169ce9307564788f930d6
ttelcl <9429819+ttelcl@users.noreply.github.com>
2022-11-25 13:12:08 +0200

    ColorLib: added HSL support

--- ColorLib/ColorDetail.cs
+++ ColorLib/ColorDetail.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using DrawingColor = System.Drawing.Color;
 
 using Newtonsoft.Json;
 
@@ -47,6 +48,14 @@ namespace ColorLib
         FractionToByte(R),
         FractionToByte(G),
         FractionToByte(B));
+      WinFormsColor = DrawingColor.FromArgb(
+        FractionToByte(R),
+        FractionToByte(G),
+        FractionToByte(B));
+      RgbToHsl(R, G, B, out var hue, out var saturation, out var lightness);
+      Hue = hue;
+      Saturation = saturation;
+      Lightness = lightness;
     }
 
     /// <summary>
@@ -79,6 +88,24 @@ namespace ColorLib
     }
 
     /// <summary>
+    /// Create from HSL values
+    /// </summary>
+    /// <param name="hue">
+    /// The hue. Range 0.0 - 360. If saturation is 0 this can be null.
+    /// </param>
+    /// <param name="saturation">
+    /// The saturation (range 0.0 - 1.0)
+    /// </param>
+    /// <param name="lightness">
+    /// The lightness (range 0.0 - 1.0)
+    /// </param>
+    public static ColorDetail FromHsl(double? hue, double saturation, double lightness)
+    {
+      HslToRgb(hue, saturation, lightness, out var r, out var g, out var b);
+      return new ColorDetail(r, g, b);
+    }
+
+    /// <summary>
     /// The red component
     /// </summary>
     [JsonProperty("r")]
@@ -97,16 +124,40 @@ namespace ColorLib
     public double B { get; }
 
     /// <summary>
-    /// The color as a System.Windows.Media.Color (WPF)
+    /// The color as a (WPF-compatible) System.Windows.Media.Color
     /// </summary>
     [JsonIgnore]
     public Color WpfColor { get; }
 
     /// <summary>
-    /// The hex color code (ignored when deserializing)
+    /// The color as a (Windows Forms compatible) System.Drawing.Color
+    /// </summary>
+    [JsonIgnore]
+    public DrawingColor WinFormsColor { get; }
+
+    /// <summary>
+    /// The hex color code. Ignored when deserializing.
     /// </summary>
     [JsonProperty("hex")]
     public string HexColor { get => ToHexString(R, G, B); }
+
+    /// <summary>
+    /// The HSL hue (range 0.0 - 360.0, or null if undefined). Ignored when deserializing.
+    /// </summary>
+    [JsonProperty("hue")]
+    public double? Hue { get; }
+
+    /// <summary>
+    /// The HSL saturation (range 0.0 - 1.0). Ignored when deserializing.
+    /// </summary>
+    [JsonProperty("saturation")]
+    public double Saturation { get; }
+
+    /// <summary>
+    /// The HSL lightness (range 0.0 - 1.0). Ignored when deserializing.
+    /// </summary>
+    [JsonProperty("lightness")]
+    public double Lightness { get; }
 
     /// <summary>
     /// Return the hex color code for an (r,g,b) triplet
@@ -138,6 +189,192 @@ namespace ColorLib
     public static double ByteToFraction(byte b)
     {
       return ((double)b)/255.0;
+    }
+
+    /// <summary>
+    /// Convert an RGB fractions triplet to a HSL triplet. 
+    /// </summary>
+    /// <param name="r">
+    /// The input red channel (range 0.0 - 1.0)
+    /// </param>
+    /// <param name="g">
+    /// The input green channel (range 0.0 - 1.0)
+    /// </param>
+    /// <param name="b">
+    /// The input blue channel (range 0.0 - 1.0)
+    /// </param>
+    /// <param name="h">
+    /// The output Hue channel (range 0.0 - 360.0; null if undefined)
+    /// </param>
+    /// <param name="s">
+    /// The output saturation channel (range 0.0 - 1.0)
+    /// </param>
+    /// <param name="l">
+    /// The output lightness channel (range 0.0 - 1.0)
+    /// </param>
+    public static void RgbToHsl(
+      double r, double g, double b,
+      out double? h, out double s, out double l)
+    {
+      // Inspired by https://www.optifunc.com/blog/hsl-color-for-wpf-and-markup-extension-to-lighten-darken-colors-in-xaml
+
+      // Get the maximum and minimum RGB components.
+      double max = r;
+      if(max < g)
+      {
+        max = g;
+      }
+      if(max < b)
+      {
+        max = b;
+      }
+
+      double min = r;
+      if(min > g)
+      {
+        min = g;
+      }
+      if(min > b)
+      {
+        min = b;
+      }
+
+      double diff = max - min;
+      l = (max + min) / 2;
+      if(Math.Abs(diff) < 0.00001)
+      {
+        s = 0;
+        h = null;  // H is really undefined.
+      }
+      else
+      {
+        if(l <= 0.5)
+        {
+          s = diff / (max + min);
+        }
+        else
+        {
+          s = diff / (2 - max - min);
+        }
+
+        double r_dist = (max - r) / diff;
+        double g_dist = (max - g) / diff;
+        double b_dist = (max - b) / diff;
+
+        double hv;
+        if(r == max)
+        {
+          hv = b_dist - g_dist;
+        }
+        else if(g == max)
+        {
+          hv = 2.0 + r_dist - b_dist;
+        }
+        else
+        {
+          hv = 4.0 + g_dist - r_dist;
+        }
+
+        hv *= 60.0;
+        if(hv < 0.0)
+        {
+          hv += 360.0;
+        }
+        if(hv >= 360.0)
+        {
+          hv -= 360.0;
+        }
+        h = hv;
+      }
+    }
+
+    /// <summary>
+    /// Convert a HSL triplet to a RGB fractions triplet
+    /// </summary>
+    /// <param name="h">
+    /// The input Hue (range 0.0 - 360.0; can be null if s==0.0)
+    /// </param>
+    /// <param name="s">
+    /// The input saturation
+    /// </param>
+    /// <param name="l">
+    /// The input lightness
+    /// </param>
+    /// <param name="r">
+    /// The output Red channel
+    /// </param>
+    /// <param name="g">
+    /// The output Green channel
+    /// </param>
+    /// <param name="b">
+    /// The output Blue channel
+    /// </param>
+    /// <exception cref="ArgumentException">
+    /// Thrown if <paramref name="h"/> is null but <paramref name="s"/> is not 0.
+    /// </exception>
+    public static void HslToRgb(
+      double? h, double s, double l,
+      out double r, out double g, out double b)
+    {
+      // Inspired by https://www.optifunc.com/blog/hsl-color-for-wpf-and-markup-extension-to-lighten-darken-colors-in-xaml
+
+      if(s == 0)
+      {
+        r = l;
+        g = l;
+        b = l;
+      }
+      else
+      {
+        if(!h.HasValue)
+        {
+          throw new ArgumentException(
+            "Expecting hue to be non-null when saturation is not zero");
+        }
+        var hv = h.Value;
+        if(hv < 0.0)
+        {
+          hv += 360;
+        }
+        if(hv >= 360.0)
+        {
+          hv -= 360;
+        }
+        var p2 = (l <= 0.5) ? l * (1 + s) : l + s - l * s;
+        var p1 = 2 * l - p2;
+        r = QqhToRgb(p1, p2, hv + 120);
+        g = QqhToRgb(p1, p2, hv);
+        b = QqhToRgb(p1, p2, hv - 120);
+      }
+    }
+
+    private static double QqhToRgb(double q1, double q2, double hue)
+    {
+      if(hue >= 360.0)
+      {
+        hue -= 360.0;
+      }
+      else if(hue < 0.0)
+      {
+        hue += 360.0;
+      }
+
+      if(hue < 60)
+      {
+        return q1 + (q2 - q1) * hue / 60;
+      }
+      else if(hue < 180)
+      {
+        return q2;
+      }
+      else if(hue < 240)
+      {
+        return q1 + (q2 - q1) * (240 - hue) / 60;
+      }
+      else
+      {
+        return q1;
+      }
     }
 
   }
--- UnitTest.ColorLib/ColorTests.cs
+++ UnitTest.ColorLib/ColorTests.cs
@@ -1,8 +1,9 @@
 using Xunit;
 using Xunit.Abstractions;
 
-using ColorLib;
 using Newtonsoft.Json;
+
+using ColorLib;
 
 namespace UnitTest.ColorLib
 {
@@ -61,6 +62,138 @@ namespace UnitTest.ColorLib
       _output.WriteLine(json);
       var color2 = JsonConvert.DeserializeObject<ColorDetail>(json);
       Assert.Equal("#4ef2b0", color2!.HexColor);
+    }
+
+    [Fact]
+    public void RgbToHlsTests()
+    {
+      // Grays
+      ColorDetail.RgbToHsl(0.0, 0.0, 0.0, out var hue, out var saturation, out var lightness);
+      Assert.Null(hue);
+      Assert.Equal(0.0, saturation);
+      Assert.Equal(0.0, lightness);
+      ColorDetail.RgbToHsl(1.0, 1.0, 1.0, out hue, out saturation, out lightness);
+      Assert.Null(hue);
+      Assert.Equal(0.0, saturation);
+      Assert.Equal(1.0, lightness);
+      ColorDetail.RgbToHsl(0.5, 0.5, 0.5, out hue, out saturation, out lightness);
+      Assert.Null(hue);
+      Assert.Equal(0.0, saturation);
+      Assert.Equal(0.5, lightness);
+
+      // Primary & Secondary colors
+      ColorDetail.RgbToHsl(1.0, 0.0, 0.0, out hue, out saturation, out lightness);
+      Assert.Equal(0.0, hue);
+      Assert.Equal(1.0, saturation);
+      Assert.Equal(0.5, lightness);
+      ColorDetail.RgbToHsl(1.0, 1.0, 0.0, out hue, out saturation, out lightness);
+      Assert.Equal(60.0, hue);
+      Assert.Equal(1.0, saturation);
+      Assert.Equal(0.5, lightness);
+      ColorDetail.RgbToHsl(0.0, 1.0, 0.0, out hue, out saturation, out lightness);
+      Assert.Equal(120.0, hue);
+      Assert.Equal(1.0, saturation);
+      Assert.Equal(0.5, lightness);
+      ColorDetail.RgbToHsl(0.0, 1.0, 1.0, out hue, out saturation, out lightness);
+      Assert.Equal(180.0, hue);
+      Assert.Equal(1.0, saturation);
+      Assert.Equal(0.5, lightness);
+      ColorDetail.RgbToHsl(0.0, 0.0, 1.0, out hue, out saturation, out lightness);
+      Assert.Equal(240.0, hue);
+      Assert.Equal(1.0, saturation);
+      Assert.Equal(0.5, lightness);
+      ColorDetail.RgbToHsl(1.0, 0.0, 1.0, out hue, out saturation, out lightness);
+      Assert.Equal(300.0, hue);
+      Assert.Equal(1.0, saturation);
+      Assert.Equal(0.5, lightness);
+
+      // non-primary saturated
+      ColorDetail.RgbToHsl(1.0, 0.5, 0.5, out hue, out saturation, out lightness);
+      Assert.Equal(0.0, hue);
+      Assert.Equal(1.0, saturation);
+      Assert.Equal(0.75, lightness);
+
+      // non-saturated
+      ColorDetail.RgbToHsl(0.75, 0.25, 0.25, out hue, out saturation, out lightness);
+      Assert.Equal(0.0, hue);
+      Assert.Equal(0.5, saturation);
+      Assert.Equal(0.5, lightness);
+      ColorDetail.RgbToHsl(0.75, 0.75, 0.25, out hue, out saturation, out lightness);
+      Assert.Equal(60.0, hue);
+      Assert.Equal(0.5, saturation);
+      Assert.Equal(0.5, lightness);
+    }
+
+    [Fact]
+    public void HlsToRgbTests()
+    {
+      // Gray
+      ColorDetail.HslToRgb(null, 0.0, 0.42, out var r, out var g, out var b);
+      Assert.Equal(0.42, r);
+      Assert.Equal(0.42, g);
+      Assert.Equal(0.42, b);
+
+      // Primaries & Secondaries
+      ColorDetail.HslToRgb(0.0, 1.0, 0.5, out r, out g, out b);
+      Assert.Equal(1.0, r);
+      Assert.Equal(0.0, g);
+      Assert.Equal(0.0, b);
+
+      ColorDetail.HslToRgb(60.0, 1.0, 0.5, out r, out g, out b);
+      Assert.Equal(1.0, r);
+      Assert.Equal(1.0, g);
+      Assert.Equal(0.0, b);
+
+      ColorDetail.HslToRgb(-300.0, 1.0, 0.5, out r, out g, out b);
+      Assert.Equal(1.0, r);
+      Assert.Equal(1.0, g);
+      Assert.Equal(0.0, b);
+
+      ColorDetail.HslToRgb(120.0, 1.0, 0.5, out r, out g, out b);
+      Assert.Equal(0.0, r);
+      Assert.Equal(1.0, g);
+      Assert.Equal(0.0, b);
+
+      ColorDetail.HslToRgb(180.0, 1.0, 0.5, out r, out g, out b);
+      Assert.Equal(0.0, r);
+      Assert.Equal(1.0, g);
+      Assert.Equal(1.0, b);
+
+      ColorDetail.HslToRgb(240.0, 1.0, 0.5, out r, out g, out b);
+      Assert.Equal(0.0, r);
+      Assert.Equal(0.0, g);
+      Assert.Equal(1.0, b);
+
+      ColorDetail.HslToRgb(-120.0, 1.0, 0.5, out r, out g, out b);
+      Assert.Equal(0.0, r);
+      Assert.Equal(0.0, g);
+      Assert.Equal(1.0, b);
+
+      ColorDetail.HslToRgb(300.0, 1.0, 0.5, out r, out g, out b);
+      Assert.Equal(1.0, r);
+      Assert.Equal(0.0, g);
+      Assert.Equal(1.0, b);
+    }
+
+    [Fact]
+    public void HslRoundtripTest()
+    {
+      var c = ColorDetail.FromHsl(0.0, 1.0, 0.5);
+      Assert.Equal(1.0, c.R);
+      Assert.Equal(0.0, c.G);
+      Assert.Equal(0.0, c.B);
+      Assert.Equal(0.0, c.Hue);
+      Assert.Equal(1.0, c.Saturation);
+      Assert.Equal(0.5, c.Lightness);
+
+      c = ColorDetail.FromHsl(42.0, 0.7, 0.6);
+      var json = JsonConvert.SerializeObject(c, Formatting.Indented);
+      _output.WriteLine(json);
+      // Some rounding errors are to be expected, so compare to limited precision
+      var precision = 12;
+      Assert.Equal(42.0, c.Hue!.Value, precision);
+      Assert.Equal(0.7, c.Saturation, precision);
+      Assert.Equal(0.6, c.Lightness, precision);
     }
   }
 }
